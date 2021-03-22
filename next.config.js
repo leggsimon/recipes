bbecaa93e81688ded6d53f237d31e19d4eb46db9
Simon Legg <leggsimon@gmail.com>
2021-03-22 19:13:35 +0100

    fix runtimecaching error

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
 	pwa: {
 		dest: 'public',
-		runtimeCaching: {
-			urlPattern: /\/_next\/image?url=.*$/i,
-			handler: 'StaleWhileRevalidate',
-			options: {
-				cacheName: 'next-image',
-				expiration: {
-					maxEntries: 32,
-					maxAgeSeconds: 24 * 60 * 60 * 2, // 48 hours
-				},
-			},
-		},
+		runtimeCaching: runtimeCaching.map((rc) => {
+			if (rc.options && rc.options.cacheName === 'static-image-assets') {
+				return {
+					...rc,
+					options: {
+						...rc.options,
+						maxAgeSeconds: 24 * 60 * 60 * 14, // 2 weeks
+					},
+				};
+			}
+		
+			return rc;
+		});,
 	},
 });
